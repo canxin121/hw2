d2f6ee79bd35d5547aac584f057ebc9e1c78cf0e
canxin <q1969730106@gmail.com>
2024-03-27 17:17:05 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,49 @@
+// The original content is temporarily commented out to allow generating a self-contained demo - feel free to uncomment later.
+
+// import 'package:flutter/material.dart';
+//
+// import 'src/app.dart';
+// import 'src/settings/settings_controller.dart';
+// import 'src/settings/settings_service.dart';
+//
+// void main() async {
+//   // Set up the SettingsController, which will glue user settings to multiple
+//   // Flutter Widgets.
+//   final settingsController = SettingsController(SettingsService());
+//
+//   // Load the user's preferred theme while the splash screen is displayed.
+//   // This prevents a sudden theme change when the app is first displayed.
+//   await settingsController.loadSettings();
+//
+//   // Run the app and pass in the SettingsController. The app listens to the
+//   // SettingsController for changes, then passes it further down to the
+//   // SettingsView.
+//   runApp(MyApp(settingsController: settingsController));
+// }
+//
+
 import 'package:flutter/material.dart';
+import 'package:hw2/src/rust/api/simple.dart';
+import 'package:hw2/src/rust/frb_generated.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+Future<void> main() async {
+  await RustLib.init();
+  runApp(const MyApp());
+}
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
+        body: Center(
+          child: Text(
+              'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
+        ),
+      ),
+    );
+  }
 }
